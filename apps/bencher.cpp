6f1dfe9a46e083e5a6fd86055d7d4b160d7958ea
R Hyde <rod.hyde@gmail.com>
2023-12-26 10:16:09 +0000

    Add "bencher" for simple benchmarking. (#24)

---
+++ apps/bencher.cpp
@@ -0,0 +1,84 @@
+#include "arviss/common/types.h"         // Address
+#include "arviss/platforms/basic/cpus.h" // BasicRv32imfCpu
+#include "arviss/rv32/concepts.h"        // IsRv32imfCpu
+
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace arviss;
+
+template<IsRv32imfCpu T>
+auto Run(T& t, size_t count) -> void
+{
+    while (count > 0 && !t.IsTrapped())
+    {
+        auto ins = t.Fetch(); // Fetch.
+        t.Dispatch(ins);      // Execute.
+        --count;
+    }
+}
+
+auto main(int argc, char* argv[]) -> int
+{
+    try
+    {
+        if (argc < 2)
+        {
+            std::cerr << "Please supply a filename.";
+            return 2;
+        }
+
+        // Read the image into a buffer.
+        const char* filename = argv[1];
+        std::ifstream fileHandle(filename, std::ios::in | std::ios::binary | std::ios::ate);
+        const std::streampos fileSize = fileHandle.tellg();
+        fileHandle.seekg(0, std::ios::beg);
+        std::vector<u8> buf(static_cast<size_t>(fileSize));
+        fileHandle.read(reinterpret_cast<char*>(buf.data()), fileSize);
+        fileHandle.close();
+
+        // Create a CPU.
+        using Cpu = Rv32imfCpuFloatCore<NoIoMem>;
+        static_assert(IsRv32imfCpu<Cpu>);
+
+        Cpu cpu{};
+
+        // Populate its memory with the contents of the image.
+        Address addr = 0;
+        for (auto b : buf)
+        {
+            cpu.Write8(addr, b);
+            ++addr;
+        }
+
+        // Execute some instructions.
+        for (int i = 0; i < 100000; i++)
+        {
+            cpu.ClearTraps();
+            cpu.SetNextPc(0);
+            Run(cpu, 1000000);
+
+            if (cpu.IsTrapped())
+            {
+                switch (cpu.TrapCause()->type_)
+                {
+                case TrapType::Breakpoint:
+                    break;
+                case TrapType::EnvironmentCallFromMMode:
+                    std::cerr << "ecall from M mode\n";
+                    break;
+                default:
+                    std::cerr << "trapped\n";
+                }
+            }
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exited with: " << e.what() << '\n';
+        return 1;
+    }
+    return 0;
+}
--- include/arviss/platforms/basic/mem.h
+++ include/arviss/platforms/basic/mem.h
@@ -7,7 +7,8 @@
 
 namespace arviss
 {
-    // A mixin implementation of a simple, checked address space that can signal bad access.
+    // A mixin implementation of a simple, checked address space that can signal bad access. It also has some simple
+    // memory-mapped I/O in the form of a TTY.
     class BasicMem
     {
         // 32KiB of memory.
@@ -65,6 +66,101 @@ namespace arviss
             else if (address == TTY_DATA)
             {
                 std::cout << static_cast<char>(byte);
+            }
+            else
+            {
+                throw TrappedException(TrapType::StoreAccessFault);
+            }
+        }
+
+        auto Write16(Address address, u16 halfWord) -> void
+        {
+            if (address < mem_.size() - 1)
+            {
+                // TODO: What if we're on a big-endian machine, or one that complains about alignment?
+                auto* p = reinterpret_cast<u16*>(&mem_[address]);
+                *p = halfWord;
+            }
+            else
+            {
+                throw TrappedException(TrapType::StoreAccessFault);
+            }
+        }
+
+        auto Write32(Address address, u32 word) -> void
+        {
+            if (address < mem_.size() - 3)
+            {
+                // TODO: What if we're on a big-endian machine, or one that complains about alignment?
+                auto* p = reinterpret_cast<u32*>(&mem_[address]);
+                *p = word;
+            }
+            else
+            {
+                throw TrappedException(TrapType::StoreAccessFault);
+            }
+        }
+    };
+
+    // A mixin implementation of a simple, checked address space that can signal bad access. It has a null
+    // implementation of memory mapped I/O for benchmarking purposes.
+    class NoIoMem
+    {
+        // 32KiB of memory.
+        std::vector<u8> mem_ = std::vector<u8>(32768);
+
+        // Memory mapped I/O.
+        const Address TTY_STATUS = 0x8000;
+        const Address TTY_DATA = 0x8001;
+
+    public:
+        auto Read8(Address address) -> u8
+        {
+            // TODO: Given that this is *checked* memory, then we could use mem_.at(address) and re-throw std::out_of_range as a TrappedException.
+            // And, yes, this is true, but the underlying code looks no better - in fact, slightly worse because it throws one exception only to
+            // call another.
+            if (address < mem_.size())
+            {
+                return mem_[address];
+            }
+            else if (address == TTY_STATUS)
+            {
+                return 1;
+            }
+            throw TrappedException(TrapType::LoadAccessFault);
+        }
+
+        auto Read16(Address address) -> u16
+        {
+            if (address < mem_.size() - 1)
+            {
+                // TODO: What if we're on a big-endian machine, or one that complains about alignment?
+                auto* p = reinterpret_cast<u16*>(&mem_[address]);
+                return *p;
+            }
+            throw TrappedException(TrapType::LoadAccessFault);
+        }
+
+        auto Read32(Address address) -> u32
+        {
+            if (address < mem_.size() - 3)
+            {
+                // TODO: What if we're on a big-endian machine, or one that complains about alignment?
+                auto* p = reinterpret_cast<u32*>(&mem_[address]);
+                return *p;
+            }
+            throw TrappedException(TrapType::LoadAccessFault);
+        }
+
+        auto Write8(Address address, u8 byte) -> void
+        {
+            if (address < mem_.size())
+            {
+                mem_[address] = byte;
+            }
+            else if (address == TTY_DATA)
+            {
+                // Do nothing.
             }
             else
             {
