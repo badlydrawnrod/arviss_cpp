32838099ab7c00e56932cef6d181779356ad73bb
R Hyde <rod.hyde@gmail.com>
2024-03-03 17:12:33 +0000

    Further removal.

--- test/source/arviss_cpp_test.cpp
+++ test/source/arviss_cpp_test.cpp
@@ -1,10 +1,1 @@
-#include <string>
-
-#include "arviss_cpp/arviss_cpp.hpp"
-
-auto main() -> int
-{
-  auto const exported = exported_class {};
-
-  return std::string("arviss_cpp") == exported.name() ? 0 : 1;
-}
+auto main() -> int { return 0; }
