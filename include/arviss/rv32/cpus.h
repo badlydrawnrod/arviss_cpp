3cf95bcb3b446cd3dd8ada00381afa8040bcd48e
R Hyde <rod.hyde@gmail.com>
2024-03-06 19:36:11 +0000

    Simplify CPUs.

--- include/arviss/rv32/cpus.h
+++ include/arviss/rv32/cpus.h
@@ -7,36 +7,20 @@
 
 namespace arviss
 {
-    // An RV32i CPU implementation for an integer core. BYO integer core. BYO memory.
-    template<IsIntegerCore T>
-    using Rv32iCpu = Rv32iDispatcher<Rv32iIntegerCoreExecutor<T>>;
-
-    // An RV32im CPU implementation for an integer core. BYO integer core. BYO memory.
-    template<IsIntegerCore T>
-    using Rv32imCpu = Rv32imDispatcher<Rv32imIntegerCoreExecutor<T>>;
-
-    // An RV32ic CPU implementation for an integer core. BYO integer core. BYO memory.
-    template<IsIntegerCore T>
-    using Rv32icCpu = Rv32icDispatcher<Rv32icIntegerCoreExecutor<T>>;
-
-    // An RV32imf CPU implementation for a floating point core. BYO floating point core. BYO memory.
-    template<IsFloatCore T>
-    using Rv32imfCpu = Rv32imfDispatcher<Rv32imfFloatCoreExecutor<T>>;
-
     // An RV32i CPU implementation for an IntegerCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32iCpuIntegerCore = Rv32iCpu<IntegerCore<Mem>>;
+    using Rv32iCpuIntegerCore = Rv32iDispatcher<Rv32iIntegerCoreExecutor<IntegerCore<Mem>>>;
 
     // An RV32im CPU implementation for an IntegerCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32imCpuIntegerCore = Rv32imCpu<IntegerCore<Mem>>;
+    using Rv32imCpuIntegerCore = Rv32imDispatcher<Rv32imIntegerCoreExecutor<IntegerCore<Mem>>>;
 
     // An RV32ic CPU implementation for an IntegerCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32icCpuIntegerCore = Rv32icCpu<IntegerCore<Mem>>;
+    using Rv32icCpuIntegerCore = Rv32icDispatcher<Rv32icIntegerCoreExecutor<IntegerCore<Mem>>>;
 
     // An RV32imf CPU implementation for a FloatCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32imfCpuFloatCore = Rv32imfCpu<FloatCore<Mem>>;
+    using Rv32imfCpuFloatCore = Rv32imfDispatcher<Rv32imfFloatCoreExecutor<FloatCore<Mem>>>;
 
 } // namespace arviss
