84f6ce4f6917bcba4e784e82d9ec22406664df1b
R Hyde <rod.hyde@gmail.com>
2024-03-06 21:01:39 +0000

    Renamed executors.

--- include/arviss/rv32/rv32.h
+++ include/arviss/rv32/rv32.h
@@ -8,18 +8,18 @@ namespace arviss
 {
     // An RV32i CPU implementation for an IntegerCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32iCpu = Rv32iDispatcher<Rv32iIntegerCoreExecutor<IntegerCore<Mem>>>;
+    using Rv32iCpu = Rv32iDispatcher<Rv32iExecutor<IntegerCore<Mem>>>;
 
     // An RV32im CPU implementation for an IntegerCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32imCpu = Rv32imDispatcher<Rv32imIntegerCoreExecutor<IntegerCore<Mem>>>;
+    using Rv32imCpu = Rv32imDispatcher<Rv32imExecutor<IntegerCore<Mem>>>;
 
     // An RV32ic CPU implementation for an IntegerCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32icCpu = Rv32icDispatcher<Rv32icIntegerCoreExecutor<IntegerCore<Mem>>>;
+    using Rv32icCpu = Rv32icDispatcher<Rv32icExecutor<IntegerCore<Mem>>>;
 
     // An RV32imf CPU implementation for a FloatCore. BYO memory.
     template<HasMemory Mem>
-    using Rv32imfCpu = Rv32imfDispatcher<Rv32imfFloatCoreExecutor<FloatCore<Mem>>>;
+    using Rv32imfCpu = Rv32imfDispatcher<Rv32imfExecutor<FloatCore<Mem>>>;
 
 } // namespace arviss
